fa52a2a5e15ea4feb5bfe01feaa53b0808b9655b
Your Name <mogolgogol.90@gmail.com>
2021-02-17 13:35:46 +0200

    layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const pictureGates = document.querySelector('.form__picture-gates');
+const contactSection = document.querySelector('.contacts__container');
+const flex = document.querySelector('.contacts__flex-wrapper');
+
+const movePic = () => {
+
+  if (window.innerWidth < 1500) {
+    pictureGates.remove();
+   contactSection.append(pictureGates);
+  } else {
+    flex.append(pictureGates);
+  }
+};
+
+movePic();
+
+window.addEventListener('resize', movePic);
